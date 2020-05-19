055b74ff9a32395f52446385ecc67f9b6531c883
bagasalwi <bagasalwisetyo2@gmail.com>
2020-05-19 10:15:26 +0700

    improvement

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -13,44 +13,41 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        PostCategory::create(
-        [
+        PostCategory::create([
             'name' => 'Game',
             'description' => 'All About Games',
-        ],
-        [
+        ]);
+        PostCategory::create([
             'name' => 'Movie',
             'description' => 'All About Movies',
-        ],
-        [
-            'name' => 'Review',
-            'description' => 'All About Review',
-        ],
-        [
+        ]);
+        PostCategory::create([
             'name' => 'Tutorial',
             'description' => 'All About Tutorial',
-        ],
-        [
+        ]);
+        PostCategory::create([
             'name' => 'Design',
             'description' => 'All About Design',
         ]);
+        PostCategory::create([
+            'name' => 'Review',
+            'description' => 'All About Review',
+        ]);
 
-        Tag::create(
-            [
-                'name' => 'Game',
-            ],
-            [
-                'name' => 'Review',
-            ],
-            [
-                'name' => 'Tutorial',
-            ],
-            [
-                'name' => 'Design',
-            ],
-            [
-                'name' => 'Movie',
-            ],
-        );
+        Tag::create([
+            'name' => 'Review',
+        ]);
+        Tag::create([
+            'name' => 'Game',
+        ]);
+        Tag::create([
+            'name' => 'Movies',
+        ]);
+        Tag::create([
+            'name' => 'Tutorial',
+        ]);
+        Tag::create([
+            'name' => 'Design',
+        ]);
     }
 }
