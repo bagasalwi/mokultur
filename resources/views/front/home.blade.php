e3f59c0e46ed1fde7ac6c87559797fa58357bf00
Bagas Alwi <bagasalwisetyo2@gmail.com>
2020-09-17 17:26:20 +0700

    17/09

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -6,7 +6,8 @@
         <div class="row">
             <div class="col-lg-5 offset-lg-1 d-none d-sm-block d-sm-block">
                 <div class="card card-body shadow rounded">
-                    <img class="img-responsive align-self-center" src="{{ asset('gambar/mock-1.png') }}" alt="" data-max-width="250px">
+                    <img class="img-responsive align-self-center" src="{{ asset('gambar/mock-1.png') }}" alt=""
+                        data-max-width="250px">
                     <h5 class="text-dark">Join Us and Start Sharing Your Creations</h5>
                     <p data-font-size="14px" class="font-weight-normal">
                         You can upload your creations from hobby, works, portofolio and sharing with others.
@@ -20,9 +21,11 @@
             <div class="col-lg-5 offset-lg-1 align-self-center">
                 <h1 class="text-dark font-weight-bold">Upload Your Creation!</h1>
                 <p class="mb-4 text-dark" data-font-size="16px">
-                    Kreasibangsa introduce creations of Anak Bangsa like Sketch, UI Design, Illustration, Reviews and more.
+                    Kreasibangsa introduce creations of Anak Bangsa like Sketch, UI Design, Illustration, Reviews and
+                    more.
                 </p>
-                <a class="btn btn-dark btn-lg font-weight-bold" href="{{ url('creation') }}" role="button">Browse Creation</a>
+                <a class="btn btn-dark btn-lg font-weight-bold" href="{{ url('creation') }}" role="button">Browse
+                    Creation</a>
             </div>
         </div>
     </div>
@@ -35,41 +38,64 @@
                 <div class="card card-full" data-background="{{ asset('gambar/bg-1.jpg') }}">
                     <div class="card-body">
                         <h4 class="text-dark">Welcome, Bagas</h4>
-                        <p class="text-dark">Kreasibangsa introduce creations of Anak Bangsa like Sketch, UI Design, Illustration, Reviews and more.</p>
+                        <p class="text-dark">Kreasibangsa introduce creations of Anak Bangsa like Sketch, UI Design,
+                            Illustration, Reviews and more.</p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6">
-                <h6 class="text-dark">Popular Creation</h6>
+                <h4 class="text-dark">Popular Creation</h4>
                 @foreach ($creation as $p)
-                    <div class="card card-hover">
-                        <div class="card-header border-bottom">
-                            <img alt="image" width="45" height="45"
-                                src="{{ URL::asset('gambar/profile_pic/' . $p->user->profile_pic) }}"
-                                class="rounded-circle mr-2">
-                            <h6><a href="{{ url('creator/' . $p->user->username) }}">{{ $p->user->name }}</a></h6>
+                <div class="card mt-2 mb-4">
+                    <img class="img-fluid img-imagepost" style="object-fit: cover;"
+                        src="{{ asset('storage/' . $p->photo()) }}" alt="">
+                    <div class="my-2">
+                        <h2><a class="text-dark" href="{{ url('creation/' . $p->slug) }}">{{ $p->title }}</a></h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 d-flex flex-row">
+                            <div class="align-self-center mr-2">
+                                <img alt="image" width="45" height="45"
+                                    src="{{ URL::asset('gambar/profile_pic/' . $p->user->profile_pic) }}"
+                                    class="rounded-circle">
+                            </div>
+                            <div class="align-self-center">
+                                <h6 class="p-0 m-0">
+                                    <a class="text-dark"
+                                        href="{{ url('creator/' . $p->user->username) }}">{{ $p->user->name }}</a>
+                                </h6>
+                                <p class="p-0 m-0"><span class="badge badge-dark">{{ $p->category->name }}</span>
+                                </p>
+                            </div>
                         </div>
-                        <div class="embed-responsive embed-responsive-4by3">
-                            <img class="embed-responsive-item img-fluid" style="object-fit: cover;"
-                                src="{{ asset('storage/' . $p->photo()) }}" alt="">
-                        </div>
-                        <div class="card-body border-top">
-                            <a class="stretched-link" href="{{ url('creation/' . $p->slug) }}">
-                                <h6>{{ $p->title }}</h6>
-                            </a>
-                        </div>
-                        <div class="card-footer">
-                            <a><i class="fas fa-eye"></i> {{ $p->view_count }}</a>
-                            <div class="bullet"></div>
-                            <a>{{ $p->category->name }}</a>
-                            <a class="float-right">{{ $p->created_at->diffForHumans() }}</a>
+                        <div class="col-6 d-flex flex-row-reverse">
+                            <div class="align-self-end">
+                                <a href="{{ url('creation/' . $p->slug) }}" class="btn btn-outline-dark ">Read More</a>
+                            </div>
                         </div>
                     </div>
-                    @endforeach
+                    {{-- <div class="card-footer">
+                        <a><i class="fas fa-eye"></i> {{ $p->view_count }}</a>
+                    <div class="bullet"></div>
+                    <a>{{ $p->category->name }}</a>
+                    <a class="float-right">{{ $p->created_at->diffForHumans() }}</a>
+                </div> --}}
             </div>
-            <div class="col-lg-3"></div>
+            @endforeach
+        </div>
+        <div class="col-lg-3">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">
+                    Cras justo odio
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+                <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item list-group-item-action">Vestibulum at eros</a>
+            </div>
         </div>
     </div>
+</div>
 </div>
 
 {{-- KREASI TERBARU --}}
@@ -90,34 +116,33 @@
                         <div class="card-header border-bottom">
                             <img alt="image" width="45" height="45"
                                 src="{{ URL::asset('gambar/profile_pic/' . $p->user->profile_pic) }}"
-                                class="rounded-circle mr-2">
-                            <h6><a href="{{ url('creator/' . $p->user->username) }}">{{ $p->user->name }}</a></h6>
-                        </div>
-                        <div class="embed-responsive embed-responsive-4by3">
-                            <img class="embed-responsive-item img-fluid"
-                                src="{{ URL::asset('gambar/user_post/' . $p->thumbnail) }}" alt="">
-                        </div>
-                        <div class="card-body border-top">
-                            <a class="stretched-link" href="{{ url('creation/' . $p->slug) }}">
-                                <h6>{{ $p->title }}</h6>
-                            </a>
-                            <div class="mt-2">
-                                <a><i class="fas fa-eye"></i> {{ $p->view_count }}</a>
-                                <div class="bullet"></div>
-                                <a>{{ $p->category->name }}</a>
-                                <a class="float-right">{{ $p->created_at->diffForHumans() }}</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            <div class="align-self-center text-center">
-                <a class="btn btn-lg btn-light mt-4" data-aos="zoom-out" data-aos-delay="600"
-                    href="{{ url('creation') }}" role="button">Lihat Selengkapnya</a>
-            </div>
-        </div>
+class="rounded-circle mr-2">
+<h6><a href="{{ url('creator/' . $p->user->username) }}">{{ $p->user->name }}</a></h6>
+</div>
+<div class="embed-responsive embed-responsive-4by3">
+    <img class="embed-responsive-item img-fluid" src="{{ URL::asset('gambar/user_post/' . $p->thumbnail) }}" alt="">
+</div>
+<div class="card-body border-top">
+    <a class="stretched-link" href="{{ url('creation/' . $p->slug) }}">
+        <h6>{{ $p->title }}</h6>
+    </a>
+    <div class="mt-2">
+        <a><i class="fas fa-eye"></i> {{ $p->view_count }}</a>
+        <div class="bullet"></div>
+        <a>{{ $p->category->name }}</a>
+        <a class="float-right">{{ $p->created_at->diffForHumans() }}</a>
     </div>
+</div>
+</div>
+</div>
+@endforeach
+</div>
+<div class="align-self-center text-center">
+    <a class="btn btn-lg btn-light mt-4" data-aos="zoom-out" data-aos-delay="600" href="{{ url('creation') }}"
+        role="button">Lihat Selengkapnya</a>
+</div>
+</div>
+</div>
 </section> --}}
 
 <section class="section">
